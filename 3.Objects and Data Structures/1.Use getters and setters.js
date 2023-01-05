3f2b6719f9ea2ff2c232109c7323ccf6b6862134
aisyah.kartina <aisyah.kartina@dataon.com>
2023-01-05 23:58:48 +0700

    recommit

--- 3.Objects and Data Structures/1.Use getters and setters.js
+++ 3.Objects and Data Structures/1.Use getters and setters.js
@@ -12,3 +12,27 @@ const account = makeBankAccount();
 account.balance = 100;
 
 // Good Code
+function makeBankAccount() {
+  // this one is private
+  let balance = 0;
+
+  // a "getter", made public via the returned object below
+  function getBalance() {
+    return balance;
+  }
+
+  // a "setter", made public via the returned object below
+  function setBalance(amount) {
+    // ... validate before updating the balance
+    balance = amount;
+  }
+
+  return {
+    // ...
+    getBalance,
+    setBalance,
+  };
+}
+
+const account = makeBankAccount();
+account.setBalance(100);
